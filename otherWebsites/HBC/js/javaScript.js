01c1cd6f5a897162fc9c6ff377b26358f3107627
AWMcBee <32169147+AWMcBee@users.noreply.github.com>
2020-02-13 15:23:07 -0500

    HBC

---
+++ otherWebsites/HBC/js/javaScript.js
@@ -0,0 +1,19 @@
+//Footer Mountains movement
+window.onscroll = function(ev) {
+	//Total Height of the page - Amount scrolled in Y direction
+	var scrollHeight = (-document.body.offsetHeight)+(window.innerHeight + window.scrollY);
+	
+	//Value Test
+	//document.getElementById("test").innerHTML = "Total Number: " + scrollHeight;
+				
+	//Left Mountains
+	document.getElementById("mountain_L_4").style.bottom = (scrollHeight*4) +"px";
+	document.getElementById("mountain_L_3").style.bottom = (scrollHeight*2.5) +"px";
+	document.getElementById("mountain_L_2").style.bottom = (scrollHeight*2) +"px";
+	document.getElementById("mountain_L_1").style.bottom = (scrollHeight*1) +"px";
+	//Right Mountains
+	document.getElementById("mountain_R_4").style.bottom = (scrollHeight*3) +"px";
+	document.getElementById("mountain_R_3").style.bottom = (scrollHeight*3.5) +"px";
+	document.getElementById("mountain_R_2").style.bottom = (scrollHeight*1) +"px";
+	document.getElementById("mountain_R_1").style.bottom = (scrollHeight*1.5) +"px";
+};
