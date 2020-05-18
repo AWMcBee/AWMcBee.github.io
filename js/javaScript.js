f7d07b65b16cf1a81d6783ccd1858be0a20d4a48
AWMcBee <32169147+AWMcBee@users.noreply.github.com>
2020-05-18 15:57:16 -0400

    FaceCrop

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,62 @@
+//Tired of constanly writing it out
+const root = document.documentElement;
+
+//Global variable for x
+//Note: Value only changes while mouse is over the image
+var z = 0;
+
+//While mouse is over the image, move the crop line.
+function mouseMove(event) {
+	//mouse x position in percentage
+	var x = (event.clientX / innerWidth)*100;
+	//Whole numbers
+	z=~~x;
+	
+	//Set value for HTML. 100-z mirrors the movement.
+	root.style.setProperty('--mouse-x', 100-z+"%");
+	
+	//Test Values
+	//document.getElementById('demoT').innerHTML = "Mouse position once leave Div: " + z;
+	
+	//Kills the centerCrop() while mouse is over the img
+	stopTime();
+}
+
+//Global variable for setInterval
+var time;
+
+//Starts the timer
+function startTime(){
+	//1000 = 1sec
+	time = setInterval(centerCrop, 10);
+}
+
+//Stops the timer
+function stopTime(){
+	clearInterval(time);
+}
+
+//Once mouse is not over the image, auto center the crop
+function centerCrop() {
+	//48% simply crops better on my face
+	if (z<48){
+		z++;
+		root.style.setProperty('--mouse-x', 100-z+"%");
+		//document.getElementById('demoT').innerHTML = "Mouse position once leave Div: " + z;
+	}
+	else if (z>48){
+		z--;
+		root.style.setProperty('--mouse-x', 100-z+"%");
+		//document.getElementById('demoT').innerHTML = "Mouse position once leave Div: " + z;
+	}
+	//Kills timer once centered
+	else if (z==48){
+		stopTime()
+	}	
+}
+
+
+// Show/Hide more text
 function myFunction(button){
 	var x = button.id;
 	switch (x) {
